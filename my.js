d4b82e9220d2b6f719fd4a97fb29dea71db0476e
tomjones92 <86981450+tomjones92@users.noreply.github.com>
2025-01-03 22:16:28 +0000

    adding js

---
+++ my.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    var pageList = [
+    'index.html'
+];
+
+    current=$(location).attr('pathname').substr($(location).attr('pathname').lastIndexOf("/")+1); // get current page: just last part - page name and extension
+    if($.inArray(current, caseList)!==-1) { //check if it is in array
+        index=$.inArray(current, caseList);
+    }
+    //navigate thorough pages...
+    $( ".next" ).click(function() {
+    if(index<caseList.length){
+    next=caseList[index+1];
+    window.location.href = next;
+    }
+    });
+
+    $( ".prev" ).click(function() {
+    if(index>=0){
+    prev=caseList[index-1];
+    window.location.href = prev;
+    }
+    });
+
+    });
